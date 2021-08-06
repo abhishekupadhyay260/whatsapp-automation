b122ebb6144b5d3e18635525db021d325430fa07
abhishekupadhyay260 <47971436+abhishekupadhyay260@users.noreply.github.com>
2021-08-07 01:22:44 +0530

    Update Whatsapp_automation.py

--- Whatsapp_automation.py
+++ Whatsapp_automation.py
@@ -1,5 +1,5 @@
 import pywhatkit 
-pywhatkit.sendwhatmsg("+9179xxxxxxx8", "just trying my whatsapp automation program", 0, 27) #here i have sent msg at 12:30am
+pywhatkit.sendwhatmsg("+9179xxxxxxx8", "just trying my whatsapp automation program", 0, 27) #here i have sent msg at 12:27am
 #firstly write phone number on which you want to send msg 
 # then write msg you want to send
 # in last write time of sending in this format (for 1:30am write  1,30  or for 5:30pm  write 17,30)
